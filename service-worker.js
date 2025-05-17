9494d8a2c908f85f7b798ee59ce0ea24ffb104a2
martindiepeveen84 <47042621+martindiepeveen84@users.noreply.github.com>
2025-05-17 21:37:39 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+// service-worker.js
+const CACHE_NAME = 'medicatieapp-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+  // Voeg hier je andere assets toe
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
+  );
+});
